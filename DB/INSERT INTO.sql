2f22f979cafe7795707154b163b4948fe2b9d20a
이민채 <118702388+lmc1996@users.noreply.github.com>
2023-03-03 17:43:11 +0900

    db파일 생성

---
+++ DB/INSERT INTO.sql
@@ -0,0 +1,9 @@
+INSERT INTO
+  MY_USER(
+    USER_ID,
+    USER_PW,
+    USER_NAME,
+    USER_NICK
+  )
+VALUES
+  ('a', 'a', 'a', 'a');
---
+++ DB/test.py
@@ -0,0 +1,82 @@
+from flask import Flask, session, render_template, redirect, request, url_for
+import cx_Oracle
+
+app = Flask(__name__)
+
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = '0000'
+app.config['MYSQL_DATABASE_DB'] = 'user_info'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.secret_key = "ABCDEFG"
+cx_Oracle.init_app(app)
+
+#연결에 필요한 기본 정보 (유저, 비밀번호, 데이터베이스 서버 주소)
+conn = cx_Oracle.connect('kgt1234','123456a','project-db-stu.ddns.net:1524/xe', encoding="UTF-8")
+
+cursor = conn.cursor()
+
+app.config['db_connection'] = conn
+
+@app.route('/', methods=['GET', 'POST'])
+def main():
+
+    if request.method == 'POST':
+        id = request.form['id']
+        pw = request.form['pw']
+ 
+        conn = cx_Oracle.connect()
+        cursor = conn.cursor()
+        sql = "SELECT * FROM my_user WHERE user_id = %s AND user_pw = %s"
+        value = (id, pw)
+        cursor.execute("set names utf8")
+        cursor.execute(sql, value)
+ 
+        data = cursor.fetchall()
+        cursor.close()
+        conn.close()
+ 
+        for row in data:
+            data = row[0]
+ 
+        if data:
+            session['login_user'] = id
+            return redirect(url_for('index'))
+        else:
+            error = 'invalid input data detected !'
+    return render_template('index.html', error = error)
+
+# @app.route('/register.html', methods=['GET', 'POST'])
+# def register():
+#     error = None
+#     if request.method == 'POST':
+#         id = request.form['regi_id']
+#         pw = request.form['regi_pw']
+ 
+#         conn = mysql.connect()
+#         cursor = conn.cursor()
+ 
+#         sql = "INSERT INTO users VALUES ('%s', '%s')" % (id, pw)
+#         cursor.execute(sql)
+ 
+#         data = cursor.fetchall()
+ 
+#         if not data:
+#             conn.commit()
+#             return redirect(url_for('main'))
+#         else:
+#             conn.rollback()
+#             return "Register Failed"
+ 
+#         cursor.close()
+#         conn.close()
+#     return render_template('register.html', error=error)
+ 
+@app.route('/index.html', methods=['GET', 'POST'])
+def index():
+    error = None
+    id = session['login_user']
+    return render_template('home.html', error=error, name=id)
+
+
+if __name__ == '__main__':
+    app.run(host = '127.0.0.1', port='5000')
