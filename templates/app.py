be7da5ebf24bca44671bccf0a0ed2345373e247d
이민채 <118702388+lmc1996@users.noreply.github.com>
2023-03-06 14:59:06 +0900

    민채 커밋

--- templates/app.py
+++ templates/app.py
@@ -1,14 +1,9 @@
-from flask import Flask, session, render_template, redirect, request, url_for
+from flask import Flask, session, escape, render_template, redirect, request, url_for
 import cx_Oracle
 
 app = Flask(__name__)
-
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = '0000'
-app.config['MYSQL_DATABASE_DB'] = 'user_info'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 app.secret_key = "ABCDEFG"
-cx_Oracle.init_app(app)
+cx_Oracle.init_oracle_client(lib_dir=r"C:\Users\777\Oracle\instantclient_21_9")
 
 #연결에 필요한 기본 정보 (유저, 비밀번호, 데이터베이스 서버 주소)
 conn = cx_Oracle.connect('kgt1234','123456a','project-db-stu.ddns.net:1524/xe', encoding="UTF-8")
@@ -17,29 +12,29 @@ cursor = conn.cursor()
 
 app.config['db_connection'] = conn
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/Login.html', methods=['GET', 'POST'])
 def login():
-
+    error=None
     if request.method == 'POST':
         id = request.form['login_id']
         pw = request.form['login_pw']
- 
+
         conn = cx_Oracle.connect()
         cursor = conn.cursor()
         sql = "SELECT * FROM my_user WHERE user_id = %s AND user_pw = %s"% (id, pw)
         value = (id, pw)
         cursor.execute("set names utf8")
         cursor.execute(sql, value)
- 
+
         data = cursor.fetchall()
         cursor.close()
         conn.close()
- 
+        
         for row in data:
             data = row[0]
- 
+
         if data:
-            session['login_user'] = id
+            session['user'] = id
             return redirect(url_for('index'))
         else:
             error = 'invalid input data detected !'
@@ -65,18 +60,27 @@ def regist():
         else:
             conn.rollback()
             return "Regist Failed"
+    cursor.close()
+    conn.close()
  
-        cursor.close()
-        conn.close()
- 
-    return render_template('Login.html', error=error)
+    return render_template('Login.html', error=error) 
  
 @app.route('/index.html', methods=['GET', 'POST'])
 def index():
     error = None
-    id = session['login_user']
-    return render_template('home.html', error=error, id=id)
+    id = session['user']
+    return render_template('index.html', error=error, id=id)
+
+
+
+# 로그아웃
+# @app.route('/out')
+# def end():
+
+#     session.pop('username', None)
+
+#     return 'end'
 
 
 if __name__ == '__main__':
-    app.run(host = '127.0.0.1', port='5000')
+    app.run(host = '127.0.0.1', port='5500')
