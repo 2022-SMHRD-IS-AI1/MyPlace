3ab8fbec1e667fa34a3e3b8d13f7bd5960e69471
이민채 <118702388+lmc1996@users.noreply.github.com>
2023-03-06 10:54:29 +0900

    민채커밋

--- DB/app.py
+++ DB/app.py
@@ -18,15 +18,15 @@ cursor = conn.cursor()
 app.config['db_connection'] = conn
 
 @app.route('/', methods=['GET', 'POST'])
-def main():
+def login():
 
     if request.method == 'POST':
-        id = request.form['id']
-        pw = request.form['pw']
+        id = request.form['login_id']
+        pw = request.form['login_pw']
  
         conn = cx_Oracle.connect()
         cursor = conn.cursor()
-        sql = "SELECT * FROM my_user WHERE user_id = %s AND user_pw = %s"
+        sql = "SELECT * FROM my_user WHERE user_id = %s AND user_pw = %s"% (id, pw)
         value = (id, pw)
         cursor.execute("set names utf8")
         cursor.execute(sql, value)
@@ -45,37 +45,37 @@ def main():
             error = 'invalid input data detected !'
     return render_template('index.html', error = error)
 
-# @app.route('/register.html', methods=['GET', 'POST'])
-# def register():
-#     error = None
-#     if request.method == 'POST':
-#         id = request.form['regi_id']
-#         pw = request.form['regi_pw']
+def regist():
+    error = None
+    if request.method == 'POST':
+        id = request.form['join_id']
+        pw = request.form['join_pw']
  
-#         conn = mysql.connect()
-#         cursor = conn.cursor()
+        conn = cx_Oracle.connect()
+        cursor = conn.cursor()
  
-#         sql = "INSERT INTO users VALUES ('%s', '%s')" % (id, pw)
-#         cursor.execute(sql)
+        sql = "INSERT INTO users VALUES ('%s', '%s')" % (id, pw)
+        cursor.execute(sql)
  
-#         data = cursor.fetchall()
+        data = cursor.fetchall()
  
-#         if not data:
-#             conn.commit()
-#             return redirect(url_for('main'))
-#         else:
-#             conn.rollback()
-#             return "Register Failed"
+        if not data:
+            conn.commit()
+            return redirect(url_for('login'))
+        else:
+            conn.rollback()
+            return "Regist Failed"
  
-#         cursor.close()
-#         conn.close()
-#     return render_template('register.html', error=error)
+        cursor.close()
+        conn.close()
+ 
+    return render_template('Login.html', error=error)
  
 @app.route('/index.html', methods=['GET', 'POST'])
 def index():
     error = None
     id = session['login_user']
-    return render_template('home.html', error=error, name=id)
+    return render_template('home.html', error=error, id=id)
 
 
 if __name__ == '__main__':
