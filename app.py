3e6a7924f37a181ffad1b44d6a976660a694b7db
이민채 <118702388+lmc1996@users.noreply.github.com>
2023-03-07 15:22:23 +0900

    uploading_file

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, session, escape, render_template, redirect, request, url_for
 import db
+import os
 
 app = Flask(__name__, template_folder='templates')
 app.secret_key = 'my_secret_key'
@@ -47,6 +48,31 @@ def index():
     id = session['user_id']
     return render_template('index.html', error=error, id=id)
 
+@app.route('/to_upload')
+def to_upload():
+    return render_template('upload.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    file = request.files['image']
+    # 'image'는 HTML 코드에서 formData.append() 메서드에서 사용한 키입니다.
+    
+    # 업로드된 파일의 저장 경로와 파일 이름을 지정합니다.
+    file_path = '/path/to/save/directory/' + file.filename
+    print(file.filename)
+    print(file_path)
+    if not os.path.exists(file_path):
+        os.makedirs(file_path)
+    # 업로드된 파일의 URL을 생성합니다.
+    url = url_for('static', filename=file.filename)
+    print(url)
+    
+    # 지정된 경로와 파일 이름으로 파일을 저장합니다.
+    file.save(file_path)
+    
+
+   # 이미지 파일을 포함하는 HTML 파일을 렌더링합니다.
+    return render_template('image.html', url=url)
 
 
 # 로그아웃
