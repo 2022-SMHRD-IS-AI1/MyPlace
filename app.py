b4b2087c73c6b08de7547dc34b7db750da7d1c9a
이민채 <118702388+lmc1996@users.noreply.github.com>
2023-03-07 17:24:33 +0900

    Update app.py

--- app.py
+++ app.py
@@ -59,20 +59,38 @@ def upload():
     
     # 업로드된 파일의 저장 경로와 파일 이름을 지정합니다.
     file_path = '/path/to/save/directory/' + file.filename
-    print(file.filename)
-    print(file_path)
+    print("file.filename: ", file.filename)
+    print("file_path :", file_path)
     if not os.path.exists(file_path):
         os.makedirs(file_path)
     # 업로드된 파일의 URL을 생성합니다.
     url = url_for('static', filename=file.filename)
-    print(url)
+    print("url :", url)
     
     # 지정된 경로와 파일 이름으로 파일을 저장합니다.
     file.save(file_path)
     
-
    # 이미지 파일을 포함하는 HTML 파일을 렌더링합니다.
     return render_template('image.html', url=url)
+
+@app.route('/properties')
+def properties():
+    return render_template('properties.html')
+
+# @app.route('/analyze', methods=['GET','POST'])
+# def analyze():
+#     url = "https://apis.openapi.sk.com/urbanbase/v1/space/analyzer"
+
+#     payload = "{\"image_path\":\"https://www.ikea.com/images/2-e4e271bd007a75af466351b6828af61c.jpg\"}"
+#     headers = {
+#         "accept": "application/json",
+#         "Content-Type": "json",
+#         "appKey": "XMGz6G5jzF5nybARW4cmY7fck4vpDiqg5u44kvRH"
+#     }
+#     response = request.post(url, data=payload, headers=headers)
+#     print(response.text)
+    
+#     return redirect()
 
 
 # 로그아웃
