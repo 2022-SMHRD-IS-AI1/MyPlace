e80239a75d552bf6473d99c6c0e91d30efde1848
이민채 <118702388+lmc1996@users.noreply.github.com>
2023-03-10 10:48:38 +0900

    upload complete

--- app.py
+++ app.py
@@ -5,12 +5,28 @@ from datetime import datetime
 import uuid
 import db
 import os
+import time
 
 
 
 app = Flask(__name__, template_folder='templates')
 CORS(app)
+app.config['UPLOAD_FOLDER'] = 'C:/Users/777/Documents/GitHub/MyPlace/static' # 업로드 파일 경로 설정
+app.config['ALLOWED_EXTENSIONS'] = {'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 app.secret_key = 'my_secret_key'
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+
+def save_file(file):
+    # 파일을 저장하고 저장된 파일의 경로를 반환합니다.
+    file_name = file.filename
+    file_ext = os.path.splitext(file_name)[1]
+    unique_name = str(int(time.time())) + file_ext
+    file_path = os.path.join(os.getcwd(), 'uploads', unique_name)
+    print('save_file_path :', file_path)
+    file.save(file_path)
+    return file_path
 
 @app.route('/')
 def go():
@@ -63,61 +79,88 @@ def to_upload():
 @app.route('/upload', methods=['GET','POST'])
 def upload():
     if request.method == 'POST':
-        # 업로드된 파일을 처리하는 로직을 작성합니다.
         file = request.files['myFileUpload']
-        # 파일을 저장하고 경로를 다른 페이지로 전달합니다.
-        file_path = save_file(file)
-        return redirect('/result?file_path=' + file_path)
+        # 업로드된 파일의 저장 경로와 파일 이름을 지정
+        now = datetime.now()
+        folder_name = now.strftime('%Y-%m-%d')
+        folder_path = os.path.join(app.root_path, 'static', folder_name)
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+        print(folder_path)
+        file_name = str(uuid.uuid4()) + '.' + file.filename.split('.')[-1]
+        file_path = os.path.join(folder_path, file_name)
+        print('file_path', file_path)
+
+        # 지정된 경로와 파일 이름으로 파일을 저장
+        file.save(file_path)
+
+        file_path.replace('\\','/')
+        print(file_path.find('static'))
+        file_path=file_path[38:]
+        
+        return redirect(url_for('analyze', file_path=file_path))
     else:
         return render_template('upload.html')
 
-@app.route('/result')
-def result():
-    file_path = request.args.get('file_path')
-    # 결과 페이지를 렌더링합니다.
-    return render_template('result.html', file_path=file_path)
 
-def save_file(file):
-    # 파일을 저장하고 저장된 파일의 경로를 반환합니다.
-    file_path = 'C:/Users/777/Documents/GitHub/MyPlace/static/images/user_img' + file.filename
-    file.save(file_path)
-    return file_path
-
-# @app.route('/image', methods=['GET', 'POST'])
+# @app.route('/image')
 # def image():
-#     return render_template('image.html')
+#     file_path = request.args.get('file_path')
+#     print('제발', file_path)
+#     # 결과 페이지를 렌더링합니다.
+#     return render_template('image.html', file_path=file_path)
 
-# @app.route('/image/<data>', methods=['GET', 'POST'])
-# def image(data):
-#     return render_template('image.html', data=data)
+@app.route('/analyze', methods=['GET','POST'])
+def analyze():
+    file_path = request.args.get('file_path')
+    url = "https://apis.openapi.sk.com/urbanbase/v1/space/analyzer"
+    # payload = "{\"image_path\":\"https://www.ikea.com/images/2-e4e271bd007a75af466351b6828af61c.jpg\"}"
+    payload = "{\"image_path\":\""+file_path+"\"}\""
+    print('Payload :',payload)
+    headers = {
+        "accept": "application/json",
+        "Content-Type": "json",
+        "appKey": "XMGz6G5jzF5nybARW4cmY7fck4vpDiqg5u44kvRH"
+    }
 
-    # if request.method == 'POST':
-    #     file = request.files['image']
-    # # 'image'는 HTML 코드에서 formData.append() 메서드에서 사용한 키
-    
-    # # 업로드된 파일의 저장 경로와 파일 이름을 지정
-    #     now = datetime.now()
-    #     folder_name = now.strftime('%Y-%m-%d')
-    #     folder_path = os.path.join(app.root_path, 'static', folder_name)
-    #     if not os.path.exists(folder_path):
-    #         os.makedirs(folder_path)
-    #     print(folder_path)
-    #     file_name = str(uuid.uuid4()) + '.' + file.filename.split('.')[-1]
-    #     file_path = os.path.join(folder_path, file_name)
-    #     print(file_path)
-    
-    # # 지정된 경로와 파일 이름으로 파일을 저장
-    #     file.save(file_path)
+    response = requests.post(url, data=payload, headers=headers)
 
-    # # 업로드된 파일의 URL을 생성
-    #     url = url_for('static', filename=os.path.join(folder_name, file_name), _external=True)
-    #     response = {
-    #         'status': 'success',
-    #         'url': url
-    #     }
-    #     return jsonify(response)
+    print('txt : ',response.text)
+    # print(response.text.find('[{"label"'))
+    # print(response.text.find('],"created_date'))
+    start = response.text.find('[{"label"')
+    end = response.text.find('],"created_date')
+    result=response.text[start:end+1]
+    # print(result.find('[{"label"'))
+    # print(result.find('},{'))
+    cut=result.find('},{')
+    result=result[:cut+1]
+    print('result : ',result)
+    
+    ### result
+    label_start = result.find('":"')
+    label_end = result.find('","')
+    label = result[label_start+3:label_end]
+    find_probability = result[label_end+2:]
+    probability_start = find_probability.find(':')+1
+    probability_end = find_probability.find(':')+8
+    probability = find_probability[probability_start:probability_end]
+    description_start = find_probability.find('on":"')+5
+    description = find_probability[description_start:]
+    description = description.replace('"}','')
+    # print(label, probability, description)
     
     
+    ### dictionary
+    data = {
+        'label' : label,
+        'probability' : probability,
+        'description' : description
+    }
+    print(data)
+    
+    return render_template('api.html', data=data, file_path=file_path)
+
     
 @app.route('/properties', methods=['GET', 'POST'])
 def properties():
